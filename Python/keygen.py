dbaeb906d1987cc5f01d6fd8888e5e043c2b1025
Cyprien <cyp.isnard@hotmail.fr>
2021-11-12 04:06:28 +0100

    Create keygen.py

---
+++ Python/keygen.py
@@ -0,0 +1,12 @@
+#made with Python 3
+import random
+
+chars = ['2','3','7','a','b','c','d','g','h','j','l','p','r','s','t','w']
+
+cd_key = []
+
+key_length = 16
+for x in range(key_length):
+    cd_key.append(random.choice(chars))
+
+print(''.join(cd_key))
