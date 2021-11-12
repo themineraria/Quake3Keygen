86b3d09da9d9bfc5e568dd4d3e9ccada6dedd829
grogcw <grogcw@gmail.com>
2021-11-12 06:23:22 +0100

    Update keygen.pl

--- Perl/keygen.pl
+++ Perl/keygen.pl
@@ -1,7 +1,6 @@
 @chars = ('2','3','7','a','b','c','d','g','h','j','l','p','r','s','t','w');
-
 my @a = (1..16);
 for(@a){
-        my $cdkey = $chars[ rand @chars ];
-        print $cdkey;
+	my $cdkey = $chars[ rand @chars ];
+	print $cdkey;
 }
