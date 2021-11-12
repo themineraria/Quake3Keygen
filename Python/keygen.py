f2b804d5eb43ffb26d0cce34b60e57f79f249de8
grogcw <grogcw@gmail.com>
2021-11-12 05:43:49 +0100

    Update keygen.py

--- Python/keygen.py
+++ Python/keygen.py
@@ -1,7 +1,6 @@
 import random
 chars = ['2','3','7','a','b','c','d','g','h','j','l','p','r','s','t','w']
 cd_key = []
-key_length = 16
-for x in range(key_length):
+for x in range(16):
     cd_key.append(random.choice(chars))
 print(''.join(cd_key))
