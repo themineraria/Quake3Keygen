f70b1ad1fc7211830206c236524a1abc68699bfb
Cyprien <cyp.isnard@hotmail.fr>
2021-11-12 05:30:19 +0100

    Update keygen.dart

--- Dart/keygen.dart
+++ Dart/keygen.dart
@@ -1,1 +1,10 @@
-
+import 'dart:math';
+void main() {
+  var rn = new Random();
+  var chars = ['2', '3', '7', 'a', 'b', 'c', 'd', 'g', 'h', 'j', 'l', 'p', 'r', 's', 't', 'w'];
+  var key = "";
+  for (int i = 0; i < 16; i++) {
+    key += chars[rn.nextInt(16)];
+  }
+  print("${key}");
+}
