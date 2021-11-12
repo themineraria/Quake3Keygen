8f43a4c5c60a7e619553850f67189540ef160e44
ethan-bmn <77893644+ethan-bmn@users.noreply.github.com>
2021-11-12 04:45:45 +0100

    Update keygen.c

--- C - C++/keygen.c
+++ C - C++/keygen.c
@@ -6,8 +6,7 @@ int main() {
   char key[16], validChars[16] = {'2', '3', '7', 'a', 'b', 'c', 'd', 'g', 'h', 'j', 'l', 'p', 'r', 's', 't', 'w'};
   srand(time(NULL));
   for (int i = 0; i < 16; i++) {
-    key[i] = validChars[rand()%((16+1)-0) + 0];
-    printf("%c", key[i]);
+    printf("%c", validChars[rand()%((16+1)-0) + 0]);
   }
 }
 
