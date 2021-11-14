d98701e9759dd34c3b0a8d479ecedb5478391f3c
Cyprien <cyp.isnard@hotmail.fr>
2021-11-14 23:13:55 +0100

    Update keygen.lisp

--- Lisp/keygen.lisp
+++ Lisp/keygen.lisp
@@ -1,9 +1,8 @@
-(defvar chars)
-(setq chars (make-array 16))
-(setq chars '(2 3 7 a b c d g h j l p r s t w))
+;; Init global random state by
+(setf *random-state* (make-random-state t))
 
-(defvar cd_key)
-(setq cd_key (make-array 16))
-
-(loop for i from 0 to 15 do (setf (aref i) cd_key //** chars[(random 16)] error here **//))
-(print cd_key)
+(loop with key = (make-string 16)
+      with chars = (make-array 16 :element-type 'character :initial-contents '(#\2 #\3 #\7 #\a #\b #\c #\d #\g #\h #\j #\l #\p #\r #\s #\t #\w))
+      for i below (length chars)
+      do (setf (cl:aref key i) (cl:aref chars (random (length chars))))
+      finally (print key))
