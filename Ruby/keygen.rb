8b01ad83d2ad20b5a6cf7c0b62a0e8112e3ea611
grogcw <grogcw@gmail.com>
2021-11-15 00:12:43 +0100

    Update keygen.rb

--- Ruby/keygen.rb
+++ Ruby/keygen.rb
@@ -1,1 +1,11 @@
+cd_key = Array.new
+chars = ['2', '3', '7', 'a', 'b', 'c', 'd', 'g', 'h', 'j', 'l', 'p', 'r', 's', 't', 'w']
+
+for i in 0..16
+  cd_key.push(chars[rand(1..16)])
+end
+puts(cd_key.join)
+
+# or if you want the riffle-shuffle one liner
+
 puts(['2', '3', '7', 'a', 'b', 'c', 'd', 'g', 'h', 'j', 'l', 'p', 'r', 's', 't', 'w'].shuffle.join)
