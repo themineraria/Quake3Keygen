e2be45d2d7a107b1dc69a87f22ae3f16618ef566
grogcw <grogcw@gmail.com>
2021-11-14 19:25:16 +0100

    Create keygen.lisp

---
+++ Lisp/keygen.lisp
@@ -0,0 +1,12 @@
+(defvar chars)
+(setq chars (make-array 16))
+(setq chars '(2 3 7 a b c d g h j l p r s t w))
+
+(defvar cd_key)
+(setq cd_key (make-array 16))
+
+(loop for i from 0 to 15
+    do (setf (aref i) cd_key chars[(random 16)])
+
+)
+(print cd_key)
