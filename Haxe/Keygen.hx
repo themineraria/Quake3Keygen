af7462d3c407fdfdbcefa76d6f9030997f18f84f
Cyklus <100812875+cyklusiguess@users.noreply.github.com>
2025-09-28 14:46:03 -0300

    Update and rename Main.hx to Keygen.hx
    bro

--- Haxe/Keygen.hx
+++ Haxe/Keygen.hx
@@ -1,4 +1,4 @@
-class Main {
+class Keygen {
     static function main() {
         var chars = ['2','3','7','a','b','c','d','g','h','j','l','p','r','s','t','w'];
         var cdKey = [for (i in 0...16) chars[Math.floor(Math.random() * chars.length)]].join("");
